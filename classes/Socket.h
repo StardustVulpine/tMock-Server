354be94d4e1a43fe6ed2181c169dbfc3d1f2e67f
Figus <99464311+StardustVulpine@users.noreply.github.com>
2026-01-10 22:42:04 +0100

    Update Socket.h
    added tybian int operator

--- classes/Socket.h
+++ classes/Socket.h
@@ -6,6 +6,7 @@
 
 #include <optional>
 #include <string>
+#include <netinet/in.h>
 
 #include "../enums/AdressFamily.h"
 #include "../enums/ConnectionType.h"
@@ -15,14 +16,16 @@ using Socket_T = int;
 namespace tmockserver {
     class Socket {
         public:
-            Socket(AdressFamily adress_family, ConnectionType connection_type);
+        Socket(AdressFamily adress_family, ConnectionType connection_type);
+        bool Bind(unsigned short int port, std::optional<std::string> ip_adress=std::nullopt);
 
-            bool Bind(unsigned short int port, std::optional<std::string> ip_adress=std::nullopt);
+        operator int() const { return m_socket; }
+
 
         private:
-            Socket_T m_socket;
-            sockaddr_in m_address{};
-            int m_adress_family;
+        Socket_T m_socket;
+        sockaddr_in m_address{};
+        int m_adress_family;
 
     };
 } // tmockserver
