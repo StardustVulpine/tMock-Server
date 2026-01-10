572ede71cfae4e59942af4f38f9d6d10fb33adae
Figus <99464311+StardustVulpine@users.noreply.github.com>
2026-01-10 22:18:58 +0100

    Create Socket class definition

---
+++ classes/Socket.h
@@ -0,0 +1,29 @@
+//
+// Created by stardustvulpine on 1/10/26.
+//
+
+#pragma once
+
+#include <optional>
+#include <string>
+
+#include "../enums/AdressFamily.h"
+#include "../enums/ConnectionType.h"
+
+using Socket_T = int;
+
+namespace tmockserver {
+    class Socket {
+        public:
+            Socket(AdressFamily adress_family, ConnectionType connection_type);
+
+            bool Bind(unsigned short int port, std::optional<std::string> ip_adress=std::nullopt);
+
+        private:
+            Socket_T m_socket;
+            sockaddr_in m_address{};
+            int m_adress_family;
+
+    };
+} // tmockserver
+
