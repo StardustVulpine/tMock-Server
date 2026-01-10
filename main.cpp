7f565b24654aff3c71af8abe106cd65000e34081
Figus <99464311+StardustVulpine@users.noreply.github.com>
2026-01-10 21:25:53 +0100

    Updated include with new path for messages.h

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
-#include "messages.h"
+#include "structs/messages.h"
 
 
 std::string errorText = "Hello Mock Terraria Server sdasdsadasda";
